10c6b225a45535734dc0fe10444b5c9a42e9b543
claudio <claudio.estay@alumno.iplacex.cl>
2026-05-23 23:14:36 -0400

    actualizaciones

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page padding>
+    <q-btn @click="access">Ingresar</q-btn>
   </q-page>
 </template>
+
+
+<script setup>
+import { api } from 'src/boot/axios'
+const access = async () => {
+  try {
+    const res = await api.post('http://localhost:5000/api/v1/auth/login', {
+      email: 'tono@alumno.com',
+      password: '123123',
+    })
+    console.log(res.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+
+
